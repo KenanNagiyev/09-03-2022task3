0296f1744e92f1e47cd8cdf27ada277a12747229
KenanNagiyev <788lnmp@code.edu.az>
2022-03-08 21:29:50 +0400

    new file

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(NumLength(num));
+        }
+        static int NumLength(int num)
+        { 
+        int count = 0;
+        int residue = 0;
+            while (num>0)
+            {
+                residue = num % 10;
+                num /= 10;
+                count++;
+            }            
+            return count;
+        }
+    }
+}
